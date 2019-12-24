6b9922145176c4cf197eedb7dfaa74e5dc9ba8af
afrolai <mantillayuan@hotmail.com>
2019-12-24 16:37:34 +0800

    Update macro.py
    add a method which is create a new module and insert the macro code

--- macro.py
+++ macro.py
@@ -1,7 +1,7 @@
 import os.path
 import win32com.client
 
-
+#VBA programs have existed in excel file
 def changeMacro(macro,file):
     if os.path.exists('./test/'+file):
         excel_macro = win32com.client.DispatchEx('Excel.Application')
@@ -20,5 +20,39 @@ def changeMacro(macro,file):
         print(macro + "_macro ran successfully!")
         del wb
         del excel_macro
+
+#add new VBA programs into excel file
+def addVBA(filename,VBAname):
+    #select sheet name is "CUSTOMER" and then control column of pivot table
+    vbacode = \
+    '''
+    Sub REGION()
+        Sheets("CUSTOMER").Select
+        ActiveSheet.PivotTables("樞紐分析表1").PivotFields("QC_REGION_GROUP").ShowDetail = False   
+    End Sub
+    '''
+
+    #to check path whether exists file
+    if os.path.exists('./test/'+filename):
+        excel = win32com.client.DispatchEx('Excel.Application')
+      #  excel.Visible = True
+        excel.DisplayAlerts = False
+        excel_path = ".\\"+filename
+        wb = excel.Workbooks.Open(Filename=excel_path)
+        ws = wb.Sheets('CUSTOMER')
+        if VBAname == 'REGION':
+            mod = wb.VBProject.VBComponents.Add(1)
+            mod.CodeModule.AddFromString(vbacode.strip())
+            excel.Run(VBAname)                     
+        else:
+            mod = wb.VBProject.VBComponents.Add(1)
+            mod.CodeModule.AddFromString(vbacode2.strip())
+            excel.Run(VBAname)             
+        wb.Save()
+        wb.Close(0)
+        excel.Quit()
+        print(VBAname+" VBA ran successfully!")
+        del wb
+        del excel
 
 
